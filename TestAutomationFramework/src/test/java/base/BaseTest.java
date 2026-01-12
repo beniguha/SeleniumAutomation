cb7709e4ca4f907b9ce7227bb159402bf9a9fc37
Benimadhab Guha <beni.guha@gmail.com>
2026-01-12 18:43:28 +0530

    Initial commit - Selenium Automation

---
+++ TestAutomationFramework/src/test/java/base/BaseTest.java
@@ -0,0 +1,54 @@
+package base;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	
+	public static WebDriver driver;
+	public static Properties prop = new Properties();
+	public static Properties loc = new Properties();
+	public static FileReader frC;
+	public static FileReader frL;
+
+	@BeforeMethod
+	public void SetUp() throws IOException {
+		
+		if(driver==null) {
+			System.out.println(System.getProperty("user.dir"));
+			frC = new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\configFiles\\config.properties");
+			frL = new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\configFiles\\locater.properties");
+			prop.load(frC);
+			loc.load(frL);
+		}
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.get(prop.getProperty("testurl"));
+		}
+		if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.get(prop.getProperty("testurl"));
+		}
+		
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+		System.out.println("Teardown Successfull");
+		
+	}
+}
